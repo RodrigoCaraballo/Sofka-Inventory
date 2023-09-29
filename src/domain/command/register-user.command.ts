8bf758e3c4404650042ebb47e21654a0cafe19ea
Rodrigo Caraballo <rodrigomcaraballo8@gmail.com>
2023-09-29 10:01:43 -0300

    ARCH

--- src/domain/command/register-user.command.ts
+++ src/domain/command/register-user.command.ts
@@ -1,4 +1,8 @@
 import { IUser } from '../interfaces/model';
 import { Command } from './command.abstract';
 
-export class RegisterUserCommand extends Command<IUser> {}
+export class RegisterUserCommand extends Command<IUser> {
+  constructor(eventData: IUser) {
+    super('USER_REGISTERED', eventData);
+  }
+}
